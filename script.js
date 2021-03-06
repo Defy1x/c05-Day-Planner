e2285c0c31d8e76473b0ec4e5e65c96f42db768a
Casandra Mcdonald <casandramcdonald@MacBook-Pro.fios-router.home>
2021-03-06 02:11:48 -0500

    everything working except save btn

--- script.js
+++ script.js
@@ -1,40 +1,58 @@
-//variables
-var savedItem = $(".saveBtn");
-//set click function on buttons and add it to local storage
+var todayDate = moment().format("[Today is] dddd, MMMM Do YYYY");
+$("#currentDay").text(todayDate)
+
+//runs on page load
+setColor();
+
+//runs first on page load
+$(document).ready(function(){
+  $('.time-block').each(function(){
+      var id = $(this).attr('data-hour');
+      var value = localStorage.getItem(id);
+      $(this).val(value);
+      console.log(value);
+  });
+});
+
+//save user input to local storage
+$('.saveBtn').on('click', function(){
+  $('.time-block').each(function(){
+      var id = $(this).attr('id');
+      var value = $(this).val();
+      localStorage.setItem(id, value);
+  });
+});
+
+//clears the local storage
+$('#clearBtn').on('click', function(){
+  $('.time-block').each(function(){
+      $('.time-block .textarea').val('');
+      var id = $(this).attr("id");
+      var value = $(this).val();
+      localStorage.setItem(id, value);
+  });
+});
 
 //set the color of the blocks
 function setColor(){
   $(".time-block").each(function(index,item){
-    // var currentHourStrng = moment().format("HH");
-    // var currentHourInt = parseInt(currentHourStrng);
-    var currentHourInt = 10;
+    var currentHourStrng = moment().format("HH");
+    var currentHourInt = parseInt(currentHourStrng);
     var timeBlockValue = parseInt($(item).attr("data-hour"));
-    console.log()
+
     if (currentHourInt === timeBlockValue){
-      $(".time-block .textarea").addClass("present");
-      console.log("present")
-      console.log (timeBlockValue);
+      $(this).children("textarea").addClass("present");
     }
     else if (currentHourInt > timeBlockValue) {
-      $(".time-block .textarea").addClass("past");
-      console.log("past");
-      console.log (timeBlockValue);
+      $(this).children("textarea").addClass("past");
     }
     else {
-      $(".time-block .textarea").addClass("future");
-      console.log("future");
-      console.log (timeBlockValue);
+      $(this).children("textarea").addClass("future");
     }
   });
 };
-
-// update the current day at the top
-var todayDate = moment().format("[Today is] dddd, MMMM Do YYYY");
-$("#currentDay").text(todayDate)
 
 // run the color check every 15 seconds
 var timeCheck = window.setInterval(function(){
   setColor();
 }, 15000);
-
-setColor();
